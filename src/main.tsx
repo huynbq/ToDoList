86f5898f7b6eaf854b224906a4ec8952d1325d89
huynbq <huy.nbq@bioturing.com>
2026-06-02 10:23:52 +0700

    feat: init tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,18 @@ import "./index.css";
 import App from "./App.tsx";
 import { StyleProvider } from "@ant-design/cssinjs";
 import { ConfigProvider } from "antd";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StyleProvider layer>
     <ConfigProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
     </ConfigProvider>
   </StyleProvider>,
 );
