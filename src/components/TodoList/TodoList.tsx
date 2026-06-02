5631d32e3de621944008dfd86e66dea158d082fa
huynbq <huy.nbq@bioturing.com>
2026-06-02 10:27:41 +0700

    feat: implement virtual scrolling for todo list

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,33 +1,64 @@
-import { Col, Flex, Row, Segmented, Space } from "antd";
+import { Button, Col, Flex, Row, Segmented, Space } from "antd";
 import { AppstoreOutlined, BarsOutlined } from "@ant-design/icons";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import TodoCard from "../TodoCard";
-import type { Todo } from "../../types/types";
-
-const todoData: Todo[] = [
-  {
-    id: 1,
-    title: "Buy groceries",
-    description: "Milk, Bread, Eggs, Butter",
-    status: "pending",
-    dueDateTime: "2024-06-30T12:00:00",
-    startDateTime: "2024-06-28T10:00:00",
-    color: "#d1e7fd",
-  },
-  {
-    id: 2,
-    title: "Finish project report",
-    description: "Complete the final report for the project",
-    status: "completed",
-    dueDateTime: "2024-06-25T12:00:00",
-    startDateTime: "2024-06-20T10:00:00",
-    color: "#fff1d6",
-  },
-];
+import { useGetTodos } from "../../hooks/queries/useTodoQueries";
+import { useVirtualizer } from "@tanstack/react-virtual";
 
 const TodoList = () => {
   const [filter, setFilter] = useState("all");
   const [view, setView] = useState("card");
+
+  const {
+    data,
+    isPending,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useGetTodos();
+
+  const todos = data?.pages.flatMap((page) => page.data) ?? [];
+
+  const parentRef = useRef<HTMLDivElement>(null);
+
+  const todoVitualizer = useVirtualizer({
+    count: hasNextPage ? todos.length + 1 : todos.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 100,
+    overscan: 10,
+  });
+
+  useEffect(() => {
+    const [lastItem] = [...todoVitualizer.getVirtualItems()].reverse();
+
+    if (!lastItem) {
+      return;
+    }
+
+    if (
+      lastItem.index >= todos.length - 1 &&
+      hasNextPage &&
+      !isFetchingNextPage
+    ) {
+      fetchNextPage();
+    }
+  }, [
+    hasNextPage,
+    fetchNextPage,
+    todos.length,
+    isFetchingNextPage,
+    todoVitualizer.getVirtualItems(),
+  ]);
+
+  if (isPending) {
+    return <p className="p-4">Loading todos...</p>;
+  }
+
+  if (error) {
+    return <p className="p-4 text-red-500">Failed to load todos.</p>;
+  }
+
   return (
     <Space vertical className="p-4">
       <Flex justify="space-between" align="center">
@@ -53,15 +84,76 @@ const TodoList = () => {
           }}
         />
       </Flex>
-      <div>
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          {todoData.map((todo) => (
-            <Col className="gutter-row" xs={24} sm={12} md={6} lg={6} xl={6}>
-              <TodoCard key={todo.id} todo={todo} />
+      <div
+        ref={parentRef}
+        style={{
+          height: "550px",
+          width: "100%",
+          overflow: "auto",
+        }}
+      >
+        <div
+          style={{
+            height: `${todoVitualizer.getTotalSize()}px`,
+            width: "100%",
+            position: "relative",
+          }}
+        >
+          {todoVitualizer.getVirtualItems().map((virtualItem) => {
+            const isLoader = virtualItem.index > todos.length - 1;
+            const todo = todos[virtualItem.index];
+
+            return (
+              <div
+                key={virtualItem.index}
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: `${virtualItem.size}px`,
+                  transform: `translateY(${virtualItem.start}px)`,
+                }}
+              >
+                {isLoader ? (
+                  hasNextPage ? (
+                    "Loading more..."
+                  ) : (
+                    "Nothing more to load"
+                  )
+                ) : (
+                  <TodoCard todo={todo} />
+                )}
+              </div>
+            );
+          })}
+        </div>
+        {/* <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} ref={parentRef}>
+          {todos.map((todo) => (
+            <Col
+              key={todo.id}
+              className="gutter-row"
+              xs={24}
+              sm={12}
+              md={6}
+              lg={6}
+              xl={6}
+            >
+              <TodoCard todo={todo} />
             </Col>
           ))}
-        </Row>
+        </Row> */}
       </div>
+      {/* {hasNextPage && (
+        <Button
+          loading={isFetchingNextPage}
+          onClick={() => {
+            void fetchNextPage();
+          }}
+        >
+          Load more
+        </Button>
+      )} */}
     </Space>
   );
 };
