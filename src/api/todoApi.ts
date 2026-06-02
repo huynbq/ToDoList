4315701e3c9799504e4aa10d8abf2c3587dc38cb
huynbq <huy.nbq@bioturing.com>
2026-06-02 10:27:16 +0700

    feat: write hook to query and mutation todos

---
+++ src/api/todoApi.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { BASE_URL } from "../constants/api";
+import type { Todo } from "../types/types";
+
+export const getTodos = async ({
+  limit = 10,
+  offset = 0,
+}: {
+  limit?: number;
+  offset?: number;
+}): Promise<Todo[]> => {
+  const { data } = await axios.get<Todo[]>(BASE_URL + "/todos", {
+    params: {
+      limit,
+      offset,
+    },
+  });
+  return data;
+};
+
+export const createTodo = async (newTodo: Omit<Todo, "id">): Promise<Todo> => {
+  const { data } = await axios.post<Todo>(BASE_URL + "/todos", newTodo);
+  return data;
+};
+
+export const editTodo = async (editTodo: Todo): Promise<Todo> => {
+  const { data } = await axios.put<Todo>(
+    `${BASE_URL}/todos/${editTodo.id}`,
+    editTodo,
+  );
+  return data;
+};
+
+export const deleteTodo = async (id: string): Promise<void> => {
+  const { data } = await axios.delete<void>(`${BASE_URL}/todos/${id}`);
+  return data;
+};
+
+export const toggleStatus = async (id: string): Promise<Todo> => {
+  const { data } = await axios.patch<Todo>(
+    `${BASE_URL}/todos/${id}/toggle-status`,
+  );
+  return data;
+};
---
+++ src/hooks/queries/useTodoQueries.ts
@@ -0,0 +1,68 @@
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { TODO_KEYS } from "../../constants/queryKeys";
+import * as todoApi from "../../api/todoApi";
+
+export const useGetTodos = () => {
+  const limit = 10;
+
+  return useInfiniteQuery({
+    queryKey: TODO_KEYS.all,
+    initialPageParam: 0,
+    queryFn: ({ pageParam }) => todoApi.getTodos({ limit, offset: pageParam }),
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length < limit) return undefined;
+
+      return allPages.length * limit;
+    },
+  });
+};
+
+export const useCreateTodo = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: todoApi.createTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: TODO_KEYS.all });
+    },
+  });
+};
+
+export const useEditTodo = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: todoApi.editTodo,
+    onSuccess: (_, varibles) => {
+      queryClient.invalidateQueries({ queryKey: TODO_KEYS.all });
+      queryClient.invalidateQueries({
+        queryKey: TODO_KEYS.detail(varibles.id),
+      });
+    },
+  });
+};
+
+export const useToggleStatus = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: todoApi.toggleStatus,
+    onSuccess: (_, varibles) => {
+      queryClient.invalidateQueries({ queryKey: TODO_KEYS.all });
+      queryClient.invalidateQueries({
+        queryKey: TODO_KEYS.detail(varibles),
+      });
+    },
+  });
+};
+
+export const useDeleteTodo = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: todoApi.deleteTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: TODO_KEYS.all });
+    },
+  });
+};
